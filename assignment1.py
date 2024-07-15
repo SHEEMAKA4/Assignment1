955ce875ad82109c76a830e0658b2aa276c07df2
SHEEMAKA4 <yassirsheemaka@gmail.com>
2024-07-15 23:40:14 +0300

    initial Message

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+number1 = int(input("Enter the first number:"))
+number2 = int(input("Enter the second number:"))
+sum = number1+ number2
+print(f"The sum of {number1} and {number2} is {sum}")
+
+
+number1 = int(input("Enter the first number:"))
+number2 = int(input("Enter the second number:"))
+sum = number1 - number2
+print(f"The sum of {number1} and {number2} is {sum}")
+
+
+number1 = int(input("Enter the first number:"))
+number2 = int(input("Enter the second number:"))
+sum = number1 * number2
+print(f"The sum of {number1} and {number2} is {sum}")
+
+
+
+
+number1 = int(input("Enter the first number:"))
+number2 = int(input("Enter the second number:"))
+sum = number1 / number2
+print(f"The sum of {number1} and {number2} is {sum}")
+
