415608e5e8023ec2d48091f52f89a42376f8cee0
SHEEMAKA4 <yassirsheemaka@gmail.com>
2024-07-15 23:51:55 +0300

    Edit difference

--- assignment1.py
+++ assignment1.py
@@ -6,8 +6,8 @@ print(f"The sum of {number1} and {number2} is {sum}")
 
 number1 = int(input("Enter the first number:"))
 number2 = int(input("Enter the second number:"))
-sum = number1 - number2
-print(f"The sum of {number1} and {number2} is {sum}")
+difference = number1 - number2
+print(f"The difference of {number1} and {number2} is {difference}")
 
 
 number1 = int(input("Enter the first number:"))
